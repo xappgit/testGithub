3d094f743c0cfd08944dee6edf8ca6a7d91ec939
Chun-Yi, Kuo <xapp.me@gmail.com>
2024-07-03 19:35:11 +0800

    1

---
+++ testGithub/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  testGithub
+//
+//  Created by Jun on 2024/7/3.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ testGithub/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  testGithub
+//
+//  Created by Jun on 2024/7/3.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
